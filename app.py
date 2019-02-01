c3b23ffb46430f0d2eea36d82417b2f8a5b780d8
Heather Drake <heatherdrake@Heathers-MacBook-Pro.local>
2019-02-01 15:29:41 -0500

    added

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import os
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    index_file = open('index.html', 'r')
+    my_html = index_file.read()
+    index_file.close()
+
+    return my_html
